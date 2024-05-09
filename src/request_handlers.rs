02ef6176b08d5ae3c565389eaac94e4da54f020d
Prince Mbeah Essilfie <mbeahessilfieprince@gmail.com>
2024-05-09 21:17:10 +0000

    implement parse form

--- src/request_handlers.rs
+++ src/request_handlers.rs
@@ -1,25 +1,37 @@
+use std::collections::HashMap;
+use std::io;
+
+use futures::future;
 use futures::future::FutureResult;
 use hyper::Chunk;
-use futures::future;
-
-
 
 pub struct NewMessage {
     pub username: String,
     pub message: String,
 }
 
-pub fn parse_form(_form_chunk: Chunk) -> FutureResult<NewMessage, hyper::Error> {
-    future::ok(NewMessage {
-        username: String::new(),
-        message: String::new(),
-    })
+pub fn parse_form(form_chunk: Chunk) -> FutureResult<NewMessage, hyper::Error> {
+    let mut form = url::form_urlencoded::parse(&form_chunk)
+        .into_owned()
+        .collect::<HashMap<String, String>>();
+    if let Some(message) = form.remove("message") {
+        let username = form.remove("username").unwrap_or(String::from("anonymous"));
+
+        future::ok(NewMessage {
+            username: String::new(),
+            message: String::new(),
+        })
+    } else {
+        future::err(hyper::Error::from(io::Error::new(io::ErrorKind::InvalidInput, "Missing field 'message'")))
+    }
 }
 
 pub fn write_to_db(_entry: NewMessage) -> FutureResult<i64, hyper::Error> {
     future::ok(0)
 }
 
-pub fn make_post_response(_result: Result<i64, hyper::Error>) -> FutureResult<hyper::Response, hyper::Error> {
+pub fn make_post_response(
+    _result: Result<i64, hyper::Error>,
+) -> FutureResult<hyper::Response, hyper::Error> {
     future::ok(hyper::Response::new().with_status(hyper::StatusCode::NotFound))
 }
