5e693f5d4419d3a7848fbd835f62f8b1d370085d
Prince Mbeah Essilfie <mbeahessilfieprince@gmail.com>
2024-05-09 21:34:42 +0000

    implement response

--- src/request_handlers.rs
+++ src/request_handlers.rs
@@ -3,7 +3,8 @@ use std::io;
 
 use futures::future;
 use futures::future::FutureResult;
-use hyper::Chunk;
+use hyper::{header::{ContentLength, ContentType}, Chunk, Response};
+use serde_json::de;
 
 pub struct NewMessage {
     pub username: String,
@@ -18,8 +19,8 @@ pub fn parse_form(form_chunk: Chunk) -> FutureResult<NewMessage, hyper::Error> {
         let username = form.remove("username").unwrap_or(String::from("anonymous"));
 
         future::ok(NewMessage {
-            username: String::new(),
-            message: String::new(),
+            username,
+            message
         })
     } else {
         future::err(hyper::Error::from(io::Error::new(io::ErrorKind::InvalidInput, "Missing field 'message'")))
@@ -33,5 +34,29 @@ pub fn write_to_db(_entry: NewMessage) -> FutureResult<i64, hyper::Error> {
 pub fn make_post_response(
     _result: Result<i64, hyper::Error>,
 ) -> FutureResult<hyper::Response, hyper::Error> {
-    future::ok(hyper::Response::new().with_status(hyper::StatusCode::NotFound))
+    match _result {
+        Ok(timestamp ) => {
+            let payload = serde_json::json!({
+                "timestamp": timestamp
+            }).to_string();
+            let response = Response::new()
+                .with_header(ContentLength(payload.len() as u64))
+                .with_header(ContentType::json())
+                .with_body(payload);
+            debug!("{:?}", response);
+            future::ok(response)
+        }
+        Err(err ) => make_error_response(err.to_string().as_str()),
+    }
+}
+
+pub fn make_error_response(error: &str) -> FutureResult<hyper::Response, hyper::Error> {
+    let payload = serde_json::json!({"error": error}).to_string();
+    let response = Response::new()
+        .with_status(hyper::StatusCode::InternalServerError)
+        .with_header(ContentLength(payload.len() as u64))
+        .with_header(ContentType::json())
+        .with_body(payload);
+    debug!("{:?}", response);
+    future::ok(response)
 }
